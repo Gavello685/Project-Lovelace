be1a976b96f412ed2e4ef7cccd767fdf77a1f696
Gavello685 <gavello685@gmail.com>
2023-12-16 11:06:00 -0600

    Added grass and cursor sprite with animations

--- MainNode.gd
+++ MainNode.gd
@@ -1,5 +1,6 @@
 extends Node
 
+@onready var _cursor = $CursorNode/AnimatedSprite2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -8,22 +9,23 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	_cursor.play("default")
 	pass
 
 
 func _drawMap():
-	var x = 0
-	var y = 0
+	var x = -16
+	var y = -16
 	for n in 5:
-		y = 0
-		x += 128
+		y = -16
+		x += 32
 		for j in 3:
-			y += 128
+			y += 32
 			var tileBody = StaticBody2D.new()
 			var tileCol = CollisionShape2D.new()
 			tileBody.add_child(tileCol)
 			var tileSprite = Sprite2D.new()
-			tileSprite.texture = load('res://icon.svg')
+			tileSprite.texture = load('res://grass.jpg')
 			tileBody.add_child(tileSprite)
 			tileBody.global_position = Vector2(x,y)
 			$MapNode.add_child(tileBody)
